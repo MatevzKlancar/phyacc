86a2b0f853b2d68cd3da58b87debd9185c5eb557
matevzklancar <matevz1998@gmail.com>
2025-01-17 10:57:02 +0100

    Added extra comments

--- src/app/lib/solana/constants.ts
+++ src/app/lib/solana/constants.ts
@@ -3,5 +3,5 @@ export const CONSTANTS = {
     process.env.NEXT_PUBLIC_SOLANA_RPC_URL || "https://api.devnet.solana.com",
   MIN_SOL_BALANCE: 2, // 2 SOL minimum requirement
   TOKEN_MINT_ADDRESS: process.env.NEXT_PUBLIC_TOKEN_MINT_ADDRESS, // Your token mint address
-  MIN_TOKEN_BALANCE: 1000, // Minimum token balance required
+  MIN_TOKEN_BALANCE: 1000, // Minimum token balance required, this is the token that is being used for the project
 };
